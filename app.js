6a510f40e2b137a855c9a651ba7da18895c46a2d
andrewkmin <andrewmin8@gmail.com>
2017-02-16 21:18:35 -0800

    1

--- app.js
+++ app.js
@@ -468,6 +468,7 @@ function receivedAccountLink(event) {
  */
 
  // RANDOM TEXT HERE TO MAKE A CHANGE
+ // more changes lmao
 
 
 function sendImageMessage(recipientId) {
@@ -479,7 +480,7 @@ function sendImageMessage(recipientId) {
       attachment: {
         type: "image",
         payload: {
-          url: "https://bitcoincharts.com/charts/bitstampUSD#rg60ztgSzm1g10zm2g25zv"
+          url: "https://bitcoincharts.com/charts/chart.png?width=940&m=bitstampUSD&SubmitButton=Draw&r=60&i=&c=0&s=&e=&Prev=&Next=&t=S&b=&a1=&m1=10&a2=&m2=25&x=0&i1=&i2=&i3=&i4=&v=1&cv=0&ps=0&l=0&p=0&"
         }
       }
     }
