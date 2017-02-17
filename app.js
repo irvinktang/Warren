2c3e2c5c3275b34b5424a3ca6d9f943c49e12572
andrewkmin <andrewmin8@gmail.com>
2017-02-17 15:33:05 -0800

    correction

--- app.js
+++ app.js
@@ -465,6 +465,7 @@ function receivedMessage(event) {
         case 'briefing':
         weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function(err, realWeather) {
           var myWeather = JSON.stringify(realWeather);
+          console.log('weather', myWeather)
           var currentTemp = myWeather.current.temperature;
           // });
           var now = new Date();
